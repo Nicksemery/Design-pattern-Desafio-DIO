c54fe1013bc54b8ae14daa5dfca0dd5033a4b144
Nicolas <nicolasrodriguesemery@gmail.com>
2025-07-30 18:11:57 -0300

    finalizado

--- Padroes-de-projetos/src/main/java/Padroes_de_projetos/service/ClienteIMP.java
+++ Padroes-de-projetos/src/main/java/Padroes_de_projetos/service/ClienteIMP.java
@@ -1,6 +1,7 @@
 package Padroes_de_projetos.service;
 
 import Padroes_de_projetos.model.Cliente;
+import Padroes_de_projetos.model.Endereco;
 import Padroes_de_projetos.repository.ClienteRepository;
 import Padroes_de_projetos.repository.EnderecoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,19 +37,31 @@ public class ClienteIMP implements ClienteService{
 
     @Override
     public void salvar(Cliente cliente) {
+        salvarClientePorCep(cliente);
+    }
+
+    private void salvarClientePorCep(Cliente cliente) {
         String cep = cliente.getEndereco().getCep();
-        enderecoRepository.findById(Long.valueOf(cep)).orElseGet(()->{
-            return null;
+        Endereco endereco = enderecoRepository.findById(Long.valueOf(cep)).orElseGet(()->{
+            Endereco novoEndereco = viaCepService.consultarCEP(cep);
+            enderecoRepository.save(novoEndereco);
+            return novoEndereco;
         });
+        cliente.setEndereco(endereco);
+        clienteRepository.save(cliente);
     }
 
     @Override
     public void alterar(Long id, Cliente cliente) {
+        Optional<Cliente> clienteBD = clienteRepository.findById(id);
+        if (clienteBD.isPresent()) {
+            salvarClientePorCep(cliente);
+        }
 
     }
 
     @Override
     public void excluir(Long id) {
-
+        clienteRepository.deleteById(id);
     }
 }
